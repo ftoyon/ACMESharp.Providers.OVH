3b8218987c3aa0243a0958fca9206f9cb4c4472b
Frederic Toyon <frederic@dice.fr>
2018-02-19 02:12:32 +0100

    Initial release.

---
+++ ACMESharp.Providers.OVH/ACMESharp.Providers.OVH-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACMESharp.Providers.OVH-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACMESharp.Providers.OVH-test")]
+[assembly: AssemblyCopyright("Copyright © Frédéric Toyon / Eugene Bekker 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4675e4be-cc36-4a37-a6f0-9c773935fb1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,11 @@
+# ACMESharp.Providers.OVH
+
+An [ACMESharp](https://github.com/ebekker/ACMESharp) provider for OVH DNS.
+
+---
+
+## Related Links
+
+Check out these other related projects and resources:
+
+[ACMESharp](https://github.com/ebekker/ACMESharp) an [ACME](https://github.com/letsencrypt/acme-spec) client library and PowerShell client for the .NET platform.
